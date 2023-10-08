5234476bc2e95cf87f68fb80995a3f18f9c0d92d
kirpichonok <ivankirpichonok@gmail.com>
2023-10-08 12:21:16 +0300

    Update Pokemon/Pokemon+CodingKey: pokemons are decoded.

--- PokemonTestProject/PokemonTestProject/Entity/Pokemon/Pokemon+CodingKeys.swift
+++ PokemonTestProject/PokemonTestProject/Entity/Pokemon/Pokemon+CodingKeys.swift
@@ -18,20 +18,39 @@ extension Pokemon {
             stringValue = String(intValue)
             self.intValue = intValue
         }
+
+        enum TypesCodingKeys: String, CodingKey {
+            case type
+
+            enum TypeNameKeys: String, CodingKey {
+                case name
+            }
+        }
     }
 
     init(from decoder: Decoder) throws {
-        self.id = nil
+        id = nil
         let container = try decoder.container(keyedBy: AnyCodingKey.self)
-        height = try container.decode(Int.self, forKey: AnyCodingKey(stringValue: "height"))
-        weight = try container.decode(Int.self, forKey: AnyCodingKey(stringValue: "weight"))
+        height = try container.decode(Int.self, forKey: AnyCodingKey(stringValue: AnyCodingKey.heightKey))
+        weight = try container.decode(Int.self, forKey: AnyCodingKey(stringValue: AnyCodingKey.weightKey))
 
-        let spritesContainer = try container.nestedContainer(keyedBy: AnyCodingKey.self, forKey: AnyCodingKey(stringValue: "sprites"))
-        image = try spritesContainer.decode(URL.self, forKey: AnyCodingKey(stringValue: "front_default"))
+        let spritesContainer = try container.nestedContainer(keyedBy: AnyCodingKey.self, forKey: AnyCodingKey(stringValue: AnyCodingKey.spritesContainerKey))
+        image = try spritesContainer.decode(URL.self, forKey: AnyCodingKey(stringValue: AnyCodingKey.imageKey))
 
-        let typesContainer = try container.nestedContainer(keyedBy: AnyCodingKey.self, forKey: AnyCodingKey(stringValue: "types"))
-        let slotOneContainer = try typesContainer.nestedContainer(keyedBy: AnyCodingKey.self, forKey: AnyCodingKey(intValue: 0))
-        let typeContainer = try slotOneContainer.nestedContainer(keyedBy: AnyCodingKey.self, forKey: AnyCodingKey(stringValue: "type"))
-        self.type = try typeContainer.decode(String.self, forKey: AnyCodingKey(stringValue: "name"))
+        var typesContainer = try container.nestedUnkeyedContainer(forKey: AnyCodingKey(stringValue: AnyCodingKey.typeContainerKey))
+        let slotContainer = try typesContainer.nestedContainer(keyedBy: AnyCodingKey.TypesCodingKeys.self)
+        let typeContainer = try slotContainer.nestedContainer(keyedBy: AnyCodingKey.TypesCodingKeys.TypeNameKeys.self, forKey: .type)
+
+        type = try typeContainer.decode(String.self, forKey: .name)
     }
+
+    // MARK: - Private interface
+}
+
+extension Pokemon.AnyCodingKey {
+    static let heightKey = "height"
+    static let weightKey = "weight"
+    static let imageKey = "front_default"
+    static let spritesContainerKey = "sprites"
+    static let typeContainerKey = "types"
 }
